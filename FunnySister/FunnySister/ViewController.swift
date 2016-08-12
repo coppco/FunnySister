df5b40135911da5a0f1f74d2e68f045818c408a5
coppco <coppco@qq.com>
2016-08-12 09:35:32 +0800

    初始化项目

---
+++ FunnySister/FunnySister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunnySister
+//
+//  Created by coco on 16/8/12.
+//  Copyright © 2016年 XHJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
